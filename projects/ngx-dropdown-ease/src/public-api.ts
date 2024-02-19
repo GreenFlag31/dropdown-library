2c8675c0018aa4837b5c7972b349af6643d724a8
GreenFlag31 <unam31@gmail.com>
2024-02-19 20:02:44 +0100

    Auto-generated commit

--- projects/ngx-dropdown-ease/src/public-api.ts
+++
@@ -1,6 +0,0 @@
-/*
- * Public API Surface of ngx-dropdown-ease
- */
-
-export * from './lib/dropdown.module';
-export * from './lib/dropdown.service';
--- src/app/app.routes.ts
+++
@@ -1,9 +0,0 @@
-import { Routes } from '@angular/router';
-import { TestSubComponent } from './test-sub/test-sub.component';
-import { MainComponent } from './main/main.component';
-
-export const routes: Routes = [
-  { path: '', pathMatch: 'full', redirectTo: 'ngx-dropdown-ease' },
-  { path: 'ngx-dropdown-ease', component: MainComponent },
-  { path: 'test', component: TestSubComponent },
-];
